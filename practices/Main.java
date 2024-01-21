602596fa91ad73ccace4f8594d9325c6708445e5
yagizkoksall <yagizkoksall@hotmail.com>
2024-01-22 02:17:25 +0300

    added new problems and solutions

--- practices/Main.java
+++ practices/Main.java
@@ -1,5 +1,6 @@
 package practices;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,5 +36,36 @@ public class Main {
         FindTheDistanceValueBetweenTwoArrays classTest = new FindTheDistanceValueBetweenTwoArrays();
         classTest.findTheDistanceValue(diziTest1,diziTest2,3);
 
+    }
+
+    // Berfin
+    public static boolean checkIfExist(int[] arr) {
+        int arrayLength = arr.length;
+        for (int i = 0 ; i < arrayLength ; i++) {
+            for (int j = 0 ; j < arrayLength ; j++) {
+                if (arr[i] == arr[j] * 2 && i!= j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Yağız
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+        StringBuilder stringNumberTest = new StringBuilder();
+        List<Integer> integerList = new ArrayList<>();
+        for (int i : num) {
+            stringNumberTest.append(i);
+        }
+
+        BigInteger bigInteger = new BigInteger(String.valueOf(stringNumberTest));
+        BigInteger bigInteger1 = new BigInteger(String.valueOf(k));
+        bigInteger =  bigInteger.add(bigInteger1);
+        String stringNumber = String.valueOf(bigInteger);
+        for (int i = 0 ; i < stringNumber.length() ; i++){
+            integerList.add(Integer.valueOf(String.valueOf(stringNumber.charAt(i))));
+        }
+        return integerList;
     }
 }
